b4c82cbca1667b0870b4b6c9d6224cb7b3fc2541
schpean <mateas.bogdan@gmail.com>
2022-05-21 14:56:25 +0300

    ISA-33 #Added interface for view services

---
+++ src/main/java/org/loose/fis/sre/controllers/ViewServicesController.java
@@ -0,0 +1,15 @@
+package org.loose.fis.sre.controllers;
+
+import javafx.fxml.FXML;
+
+public class ViewServicesController {
+
+    @FXML
+    protected void onSoareluiButtonClick(){
+    }
+
+    @FXML
+    protected void onSaguluiButtonClick(){
+
+    }
+}
